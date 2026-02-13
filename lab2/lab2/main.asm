5d9df3d9ab9c80b31e79393c352069013af416e3
Andres Barrientos <124892853+andresobb@users.noreply.github.com>
2026-02-13 11:18:25 -0600

    prelaboratorio 2 terminado
    funciona a la perfección, o eso creo xd

--- lab2/lab2/main.asm
+++ lab2/lab2/main.asm
@@ -21,15 +21,62 @@ LDI     R16, HIGH(RAMEND)
 OUT     SPH, R16
 /****************************************/
 // Configuracion MCU
-SETUP:
+
+LDI		R16, (1 << CLKPCE)
+STS		CLKPR, R16
+LDI		R16, 0x04		
+STS		CLKPR, R16
+
+LDI		R16, 0x0F			//contador cada 100ms en puerto C
+OUT		DDRC, R16
+LDI		R16, 0x00
+OUT		PORTC, R16
+
+.def CONTADOR = R23
+.def OVERFLOW = R24
+
+CLR	CONTADOR
+CLR	OVERFLOW
+
+CALL INIT_TMR0
+
     
 /****************************************/
 // Loop Infinito
 MAIN_LOOP:
-    RJMP    MAIN_LOOP
+
+	ESPERA_OVF:
+		IN		R16, TIFR0
+		SBRS	R16, TOV0		//bit 0 de TIFR0
+		RJMP	ESPERA_OVF
+
+		SBI		TIFR0, TOV0
+		LDI		R16, 100
+		OUT		TCNT0, R16
+
+		INC		OVERFLOW
+		CPI		OVERFLOW, 10
+
+		BRNE	ESPERA_OVF
+		CLR		OVERFLOW
+
+		INC		CONTADOR
+		ANDI	CONTADOR, 0x0F
+		OUT		PORTC, CONTADOR
+		
+		RJMP    MAIN_LOOP
 
 /****************************************/
 // NON-Interrupt subroutines
+
+INIT_TMR0:		;usamos prescaler de 64
+	LDI		R16, (1 << CS01) | (1 << CS00)
+	OUT		TCCR0B, R16
+
+	LDI		R16, 100
+	OUT		TCNT0, R16
+
+	RET
 
 /****************************************/
 // Interrupt routines
