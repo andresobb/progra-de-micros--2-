532f27ba968d65133af76d50be0015677331db60
Andres Barrientos <124892853+andresobb@users.noreply.github.com>
2026-02-13 18:51:05 -0600

    lab 2 - no terminado
    hay que revisar valores de tabla y decremento

--- lab2/lab2/main.asm
+++ lab2/lab2/main.asm
@@ -13,6 +13,9 @@
 
 .cseg
 .org 0x0000
+
+segdig: .dB 0x40, 0x79, 0x24, 0x30, 0x24, 0x19, 0x24, 0x02, 0x78, 0x00, 0x18, 0x08, 0x03, 0x31, 0x21, 0x06, 0x0E
+
  /****************************************/
 // Configuración de la pila
 LDI     R16, LOW(RAMEND)
@@ -27,24 +30,39 @@ STS		CLKPR, R16
 LDI		R16, 0x04		
 STS		CLKPR, R16
 
-LDI		R16, 0x0F			//contador cada 100ms en puerto C
+LDI		R16, 0x0F  			//contador cada 10ms en puerto C
 OUT		DDRC, R16
 LDI		R16, 0x00
 OUT		PORTC, R16
 
+LDI		R16, 0xFF
+OUT		DDRD, R16
+LDI		R16, 0x00
+OUT		PORTD, R16
+
+LDI		R16, 0x00
+OUT		DDRB, R16
+LDI		R16, 0x03
+OUT		PORTB, R16
+
 .def CONTADOR = R23
 .def OVERFLOW = R24
+.def CONTADOR7SEG = R25
+.def BOTONES = R26
 
 CLR	CONTADOR
 CLR	OVERFLOW
+CLR CONTADOR7SEG
+CLR	R0
+
+IN	BOTONES, PINB
 
 CALL INIT_TMR0
-
     
 /****************************************/
 // Loop Infinito
 MAIN_LOOP:
-
+	
 	ESPERA_OVF:
 		IN		R16, TIFR0
 		SBRS	R16, TOV0		//bit 0 de TIFR0
@@ -55,16 +73,20 @@ MAIN_LOOP:
 		OUT		TCNT0, R16
 
 		INC		OVERFLOW
-		CPI		OVERFLOW, 10
+		CPI		OVERFLOW, 100
 
-		BRNE	ESPERA_OVF
+		BRNE	FINMAIN
 		CLR		OVERFLOW
 
 		INC		CONTADOR
 		ANDI	CONTADOR, 0x0F
 		OUT		PORTC, CONTADOR
 		
-		RJMP    MAIN_LOOP
+		RJMP    FINMAIN
+
+FINMAIN:
+	CALL	BOTONES_fnc
+	RJMP	MAIN_LOOP
 
 /****************************************/
 // NON-Interrupt subroutines
@@ -77,6 +99,63 @@ INIT_TMR0:		;usamos prescaler de 64
 	OUT		TCNT0, R16
 
 	RET
+
+BOTONES_fnc:
+	IN		R16, PINB
+	CP		BOTONES, R16
+	BREQ	FIN_BOTONES
+
+	CALL	DELAY
+
+	IN		R16, PINB
+	CP		BOTONES, R16
+	BREQ	FIN_BOTONES
+
+	MOV		BOTONES, R16
+
+	SBRS	BOTONES, 0
+	CALL	INC_CONTADOR7SEG
+
+	SBRS	BOTONES, 1
+	CALL	DEC_CONTADOR7SEG
+
+	LDI		ZH, HIGH(segdig << 1)
+	LDI		ZL, LOW(segdig << 1)
+
+	ADD		ZL, CONTADOR7SEG
+	ADC		ZH, R0
+	LPM		R19, Z
+	OUT		PORTD, R19
+	
+	RET
+
+FIN_BOTONES:
+	RET
+
+INC_CONTADOR7SEG:
+	INC		CONTADOR7SEG
+	CPI		CONTADOR7SEG, 16
+	BRNE	INC_LISTO
+	CLR		CONTADOR7SEG
+
+INC_LISTO:
+	RET
+
+DEC_CONTADOR7SEG:
+	DEC		CONTADOR7SEG
+	CPI		CONTADOR7SEG, 255
+	BRNE	DEC_LISTO
+	LDI		CONTADOR7SEG, 15
+
+DEC_LISTO:
+	RET
+
+DELAY:
+	LDI		R20, 0xFF
+DELAY_1:
+	DEC		R20
+	BRNE	DELAY_1
+	RET			
 
 /****************************************/
 // Interrupt routines
