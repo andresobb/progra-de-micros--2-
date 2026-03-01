04fc40f4fb952d8431e810f420359865c5fac4b2
Andres Barrientos <124892853+andresobb@users.noreply.github.com>
2026-03-01 01:49:43 -0600

    cambiamos entre hora y fecha
    MIIIILES de cambios pero ya podemos cambiar entre hora y fecha con PC3, y que siga contando

--- Proyecto1_Reloj/Proyecto1_Reloj/main.asm
+++ Proyecto1_Reloj/Proyecto1_Reloj/main.asm
@@ -11,12 +11,41 @@
 .dseg
 .org    SRAM_START
 
+KEY_LAST: .byte 1      ; estado anterior de botones (para flanco)
+
+CH_HOR_D: .byte 1
+CH_HOR_U: .byte 1
+CH_MIN_D: .byte 1
+CH_MIN_U: .byte 1
+
+CF_DIA_D: .byte 1
+CF_DIA_U: .byte 1
+CF_MES_D: .byte 1
+CF_MES_U: .byte 1
+
+DIA_U: .byte 1
+DIA_D: .byte 1
+MES_U: .byte 1
+MES_D: .byte 1
+
+.cseg
+
 .equ T1VALUE = 0xC2F7		//49,911 PARA 1s
 .equ M_HORA = 0
 .equ M_FECHA = 1
 .equ M_CONF_HORA = 2
 .equ M_CONF_FECHA = 3
 .equ M_CONF_ALARMA = 4
+.equ BTN_SAVE_BIT = 3   ; PC3
+.equ BTN_A_BIT    = 4   ; PC4
+.equ BTN_B_BIT    = 5   ; PC5
+
+.equ BTN_MASK     = (1<<PC3)|(1<<PC4)|(1<<PC5)
+
+.equ EV_1S       = 0   ; tick de 1 segundo (solo si NO estás en config)
+.equ EV_SAVE     = 1   ; botón PC3
+.equ EV_INC_A    = 2   ; botón PC4
+.equ EV_INC_B    = 3   ; botón PC5
 
 .def TICKS = R18
 .def SEG = R20
@@ -28,13 +57,8 @@
 .def DIGITO	= R26
 .def SALIDA7 = R27
 .def FLAGS = R28		//bit 0 (1 - alarma activa, 0 - inactiva), bit1 si esta seteada o no
-.def DIA_U = R17
-.def DIA_D = R19
-.def MES_U = R29
-.def MES_D = R30
-.def MODE = R31
+.def MODE = R19
 
-.cseg
 .org 0x0000
 	RJMP	SETUP
 .org OVF0addr		//0x0020
@@ -71,15 +95,15 @@ OUT		DDRB, R16			//PB0-PB3 multiplex y PB4 BUZZER
 LDI		R16, 0x00
 OUT		PORTB, R16
 
-LDI		R16, 0x38			//0011 1000	; PC0 - PC2 botones, PC3 - P5 LEDS DE ESTADOS
+LDI		R16, 0x07
 OUT		DDRC, R16
-LDI		R16, 0x07			// pullups
+LDI		R16, 0x38	// pullups
 OUT		PORTC, R16	
 
 LDI		R16, (1 << PCIE1)
 STS		PCICR, R16
 
-LDI		R16, (1 << PCINT10) | (1 << PCINT9) | (1 << PCINT8)
+LDI		R16, (1 << PCINT13) | (1 << PCINT12) | (1 << PCINT11)
 STS		PCMSK1, R16
 
 CLR		SEG
@@ -88,15 +112,20 @@ CLR		MIN_U
 CLR		MIN_D
 CLR		HOR_U
 CLR		HOR_D	
-CLR		DIA_U
-CLR		DIA_D
-CLR		MES_U
-CLR		MES_D
 CLR		DIGIDX
 CLR		DIGITO
 CLR		SALIDA7
 CLR		MODE
 CLR		R0
+
+LDI R16, 0
+STS DIA_U, R16
+STS DIA_D, R16
+STS MES_U, R16
+STS MES_D, R16
+
+LDI		R16, BTN_MASK
+STS		KEY_LAST, R16
 
 CALL INIT_TMR0
 
@@ -109,7 +138,31 @@ SEI
 /****************************************/
 // Loop Infinito
 MAIN_LOOP:
-	RJMP    MAIN_LOOP
+	SBRS	FLAGS, EV_SAVE
+	RJMP    CHECK_1S
+
+	CBR		FLAGS, (1 << EV_SAVE)		//limpiamos la bandera
+
+	CPI		MODE, M_HORA
+	BREQ	TO_FECHA
+	CPI		MODE, M_FECHA
+	BREQ	TO_HORA
+	RJMP	CHECK_1S
+
+TO_FECHA:
+	LDI		MODE, M_FECHA
+	RJMP	CHECK_1S
+
+TO_HORA:
+	LDI		MODE, M_HORA
+
+CHECK_1S:
+	SBRS	FLAGS, EV_1S
+	RJMP	MAIN_LOOP
+
+	CBR		FLAGS, (1 << EV_1S)
+	CALL	TICK_1S
+	RJMP	MAIN_LOOP
 
 /****************************************/
 // NON-Interrupt subroutines
@@ -121,6 +174,43 @@ INIT_TMR0:		;usamos prescaler de 64, esto sera para el multiplexado
 	LDI		R16, 178		//aprox 5ms
 	OUT		TCNT0, R16
 
+	RET
+
+TICK_1S:
+
+	SBI		PINB, PINB5
+
+	INC		SEG
+	CPI		SEG, 60
+	BRNE	TICK_END
+	CLR		SEG
+
+	//minutos
+	INC		MIN_U
+	CPI		MIN_U, 10
+	BRNE	TICK_END
+	CLR		MIN_U
+	INC		MIN_D
+	CPI		MIN_D, 6
+	BRNE	TICK_END
+	CLR		MIN_D
+
+	//horas
+	INC		HOR_U
+	CPI		HOR_U, 10
+	BRNE	FIX_24
+	CLR		HOR_U
+	INC		HOR_D
+
+	FIX_24:
+	CPI		HOR_D, 2
+	BRNE	TICK_END
+	CPI		HOR_U, 4
+	BRNE	TICK_END
+	CLR		HOR_D
+	CLR		HOR_U
+
+	TICK_END:
 	RET
 
 /****************************************/
@@ -140,44 +230,17 @@ TIMER0_OVF:
 
 	INC		TICKS
 	CPI		TICKS, 200			// 200 * 5ms = 1s
-	BRNE	MUX_START
+	BRNE	DO_MUX
 	CLR		TICKS
 
-	SBI		PINB, PINB5
+	CPI		MODE, M_CONF_HORA
+	BREQ	DO_MUX
+	CPI		MODE, M_CONF_FECHA
+	BREQ	DO_MUX
 
-	INC		SEG
-	CPI		SEG, 60
-	BRNE	MUX_START
-	CLR		SEG
+	SBR		FLAGS, (1 << EV_1S)		//si no esta configurando, genera el segundo
 
-	//minutos
-	INC		MIN_U
-	CPI		MIN_U, 10
-	BRNE	MUX_START
-	CLR		MIN_U
-	INC		MIN_D
-	CPI		MIN_D, 6
-	BRNE	MUX_START
-	CLR		MIN_D
-
-	//horas
-	INC		HOR_U
-	CPI		HOR_U, 10
-	BRNE	FIX_24
-	CLR		HOR_U
-	INC		HOR_D
-
-	FIX_24:
-	CPI		HOR_D, 2
-	BRNE	MUX_START
-	CPI		HOR_U, 4
-	BRNE	MUX_START
-	CLR		HOR_D
-	CLR		HOR_U
-
-	//hacer aca lo de que llegue a 24h y cambiar dia
-
-MUX_START:
+	DO_MUX:
 
 	CBI		PORTB, PORTB0
 	CBI		PORTB, PORTB1
@@ -186,9 +249,15 @@ MUX_START:
 
 	CPI		MODE, M_HORA
 	BREQ	SHOW_HORA
-	RJMP	SHOW_FECHA
+	CPI		MODE, M_FECHA
+	BREQ	SHOW_FECHA
 
-SHOW_HORA:
+	CPI		MODE, M_CONF_HORA
+	BREQ	SHOW_HORA
+	CPI		MODE, M_CONF_FECHA
+	BREQ	SHOW_FECHA
+
+	SHOW_HORA:
 	CPI		DIGIDX, 0
 	BREQ	D0
 	CPI		DIGIDX, 1
@@ -197,27 +266,27 @@ SHOW_HORA:
 	BREQ	D2
 	RJMP	D3		//tipo else
 
-D0:									//decimal hora
+	D0:									//decimal hora
 	MOV		DIGITO, HOR_D
 	SBI		PORTB, PORTB0
 	RJMP	SHOW
 
-D1:									//unidad hora
+	D1:									//unidad hora
 	MOV		DIGITO, HOR_U
 	SBI		PORTB, PORTB1
 	RJMP	SHOW
 
-D2:									//decimal minuto
+	D2:									//decimal minuto
 	MOV		DIGITO, MIN_D
 	SBI		PORTB, PORTB2
 	RJMP	SHOW
 
-D3:									//unidad minuto
+	D3:									//unidad minuto
 	MOV		DIGITO, MIN_U
 	SBI		PORTB, PORTB3
 	RJMP	SHOW
 
-SHOW_FECHA:
+	SHOW_FECHA:
 	CPI		DIGIDX, 0
 	BREQ	F0
 	CPI		DIGIDX, 1
@@ -226,28 +295,28 @@ SHOW_FECHA:
 	BREQ	F2
 	RJMP	F3		//tipo else
 
-F0:									//decimal hora
-	MOV		DIGITO, DIA_D
+	F0:									//decimal hora
+	LDS		DIGITO, DIA_D
 	SBI		PORTB, PORTB0
 	RJMP	SHOW
 
-F1:									//unidad hora
-	MOV		DIGITO, DIA_U
+	F1:									//unidad hora
+	LDS		DIGITO, DIA_U
 	SBI		PORTB, PORTB1
 	RJMP	SHOW
 
-F2:									//decimal minuto
-	MOV		DIGITO, MES_D
+	F2:									//decimal minuto
+	LDS		DIGITO, MES_D
 	SBI		PORTB, PORTB2
 	RJMP	SHOW
 
-F3:									//unidad minuto
-	MOV		DIGITO, MES_U
+	F3:									//unidad minuto
+	LDS		DIGITO, MES_U
 	SBI		PORTB, PORTB3
 	RJMP	SHOW
 
 
-SHOW:
+	SHOW:
 	LDI		ZH, HIGH(segdig << 1)
     LDI		ZL, LOW(segdig << 1)
     ADD		ZL, DIGITO
@@ -260,7 +329,7 @@ SHOW:
 	BRNE	SALIDA_ISR
 	CLR		DIGIDX
 
-SALIDA_ISR:
+	SALIDA_ISR:
 	POP		R16
 	OUT		SREG, R16
 	POP		R31
@@ -273,24 +342,40 @@ SALIDA_ISR:
 
 PORTC_ISR:
 	PUSH	R16
+	PUSH	R17
+	PUSH	R15
 	IN		R16, SREG
 	PUSH	R16
-	
-	CBI		PORTB, PORTB4		//boton apaga al buzzer
 
 	IN		R16, PINC
+	ANDI	R16, BTN_MASK
 
-	SBRS	R16, 0
-	RJMP	BTN0
+	LDS		R17, KEY_LAST
+	MOV		R15, R16
+	COM		R15
+	AND		R15, R17
+
+	STS		KEY_LAST, R16
+
+	SBRS	R15, BTN_SAVE_BIT
+	RJMP	CHECK_A
+	SBR		FLAGS, (1 << EV_SAVE)
+
+	CHECK_A:
+	SBRS	R15, BTN_A_BIT
+	RJMP	CHECK_B
+	SBR		FLAGS, (1 << EV_INC_A)
+
+	CHECK_B:
+	SBRS	R15, BTN_B_BIT
 	RJMP	END_ISR
-
-BTN0:
-	LDI		R16, 1
-	EOR		MODE, R16
-
-END_ISR:
+	SBR		FLAGS, (1 << EV_INC_B)
+	
+	END_ISR:
 	POP		R16
 	OUT		SREG, R16
+	POP		R15
+	POP		R17
 	POP		R16
 
 	RETI
