33a0dd41fa93bdabe9f637c0fcf84aa909f1e7e2
Andres Barrientos <124892853+andresobb@users.noreply.github.com>
2026-04-16 10:10:04 -0600

    se crearon las librerías

---
+++ lab5/lab5/PWM1/PWM1.c
@@ -0,0 +1,6 @@
+/*
+ * PWM1.c
+ *
+ * Created: 4/16/2026 10:09:35 AM
+ *  Author: andre
+ */ 
---
+++ lab5/lab5/PWM1/PWM1.h
@@ -0,0 +1,16 @@
+/*
+ * PWM1.h
+ *
+ * Created: 4/16/2026 10:09:19 AM
+ *  Author: andre
+ */ 
+
+
+#ifndef PWM1_H_
+#define PWM1_H_
+
+
+
+
+
+#endif /* PWM1_H_ */
