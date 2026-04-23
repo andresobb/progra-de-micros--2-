6c8280a42850a6101eecd8a0a1e46f63e6caf304
Andres Barrientos <124892853+andresobb@users.noreply.github.com>
2026-04-23 12:30:59 -0600

    documento generado
    solo se creó el archivo para actualizar git

---
+++ lab6/lab6/main.c
@@ -0,0 +1,31 @@
+/*
+ * lab6.c
+ *
+ * Created: 4/23/2026 12:25:32 PM
+ * Author: Andrés Barrientos
+ * Description: 
+ */
+/****************************************/
+// Encabezado (Libraries)
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
+
+/****************************************/
+// Function prototypes
+
+/****************************************/
+// Main Function
+int main(void)
+{
+	/* Replace with your application code */
+	while (1)
+	{
+	}
+}
+
+/****************************************/
+// NON-Interrupt subroutines
+
+/****************************************/
+// Interrupt routines
