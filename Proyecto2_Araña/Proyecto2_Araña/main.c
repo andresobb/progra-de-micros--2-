0d217c83e9baf2dd38a32243d95f970696632649
Andres Barrientos <124892853+andresobb@users.noreply.github.com>
2026-04-30 16:31:44 -0600

    se creo el documento

---
+++ Proyecto2_Araña/Proyecto2_Araña/main.c
@@ -0,0 +1,36 @@
+/*
+ * Proyecto2_Araña.c
+ *
+ * Created:4/30/2026 4:26:11 PM 
+ * Author: Andrés Barrientos
+ * Description: Araña con servomotores
+ */
+/****************************************/
+// Encabezado (Libraries)
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
+
+/****************************************/
+// Function prototypes
+void setup(void);
+/****************************************/
+// Main Function
+
+int main(void)
+{
+	/* Replace with your application code */
+	while (1)
+	{
+	}
+}
+
+/****************************************/
+// NON-Interrupt subroutines
+void setup(void)
+{
+	
+}
+
+/****************************************/
+// Interrupt routines
